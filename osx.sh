f435fde2d1a88ecf30c9496a2d924bef3907bc05
Francis Chartrand <chartrandf@bonyweb.com>
2013-04-02 21:48:15 -0400

    Ajust oxs.sh file

--- osx.sh
+++ osx.sh
@@ -1,5 +1,10 @@
 # Repeat delay
-defaults write NSGlobalDomain InitialKeyRepeat -int 50
+defaults write NSGlobalDomain InitialKeyRepeat -int 11
 
-# faster key repeat
+# Faster key repeat
 defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Disable press and hold funtions
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+echo "OSX global vars configured"
