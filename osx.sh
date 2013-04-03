7932a2ef7b34d01f638c7a1c6eb3e1efd6eed1b9
Francis Chartrand <chartrandf@bonyweb.com>
2013-04-03 08:31:32 -0400

    Add a new osx configuration

--- osx.sh
+++ osx.sh
@@ -7,4 +7,7 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 # Disable press and hold funtions
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Use current directory as default search scope in Finder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 echo "OSX global vars configured"
