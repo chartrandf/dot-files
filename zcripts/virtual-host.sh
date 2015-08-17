637ae83726d076b82e2ce83e14284b130af1a6e3
Francis Chartrand <chartrandf@bonyweb.com>
2015-08-17 16:21:20 -0400

    Add bash script for a quick virtual-host edit

---
+++ zcripts/virtual-host.sh
@@ -0,0 +1,8 @@
+printf "\e[0;32mOpen all files in sublime text 2!\e[0m \n"
+
+subl /private/etc/apache2/extra/httpd-vhosts.conf
+sleep 1
+subl /etc/hosts
+
+printf "\e[0;32mDon't forget: \e[0m sudo apachectl restart\n"
+
