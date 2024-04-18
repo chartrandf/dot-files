1b0f926df087400ed1ff3ab6ab852c496231a910
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-04-18 15:10:21 -0400

    fix(nvm): auto setup

--- nvm/configure.sh
+++ nvm/configure.sh
@@ -1,8 +1,14 @@
 cd nvm
 
-if command -v "nvm" >/dev/null 2>&1; then
+if type nvm &> /dev/null; then
+  echo "NVM already installed"
+else
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
   rm install.sh || true
+  
+  # ZSH auto load from .nvmrc
+  cp load-nvmrc.sh ~/scripts
+  echo "source ~/scripts/load-nvmrc.sh" >> ${ZDOTDIR:-$HOME}/.zshrc
 fi
 
 cd ..
