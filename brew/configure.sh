1e64e47a6cc83e5405a7bb831098903af9e0185d
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-04-19 10:01:14 -0400

    feat: install needed application

--- brew/configure.sh
+++ brew/configure.sh
@@ -21,15 +21,16 @@ fi
 brew bundle --file=- <<EOF
 # Unix tools
 brew "git"     # Install git
+brew "tig"     # Install tig
 brew "gnupg"   # GnuPG for signed commit
 brew "vim"     # Install vim
 
 # Helpers
 brew "git-delta"   # Install delta: Delta a better diff tool
-brew "httpie"  # Install httpie: Modern, user-friendly command-line HTTP client for the API era.
-brew "gh"      # Install gh: GitHub command-line interface
-brew "jq"      # Install jq: Command-line JSON processor
-brew "gh"      # Intall gh: Github CLI
+brew "httpie"      # Install httpie: Modern, user-friendly command-line HTTP client for the API era.
+brew "gh"          # Install gh: GitHub command-line interface
+brew "jq"          # Install jq: Command-line JSON processor
+brew "gh"          # Intall gh: Github CLI
 
 # Ruby
 brew "rbenv"
@@ -41,6 +42,19 @@ brew "ruby-build"
 # Database
 brew "postgresql@16", restart_service: :changed
 brew "mysql"
+
+# Applications
+cask "rectangle"            # https://rectangleapp.com/
+cask "raycast"              # https://www.raycast.com/
+cask "visual-studio-code"   # https://code.visualstudio.com/
+cask "beekeeper-studio"     # https://www.beekeeperstudio.io/
+cask "bitwarden"            # https://bitwarden.com/
+cask "docker"               # https://www.docker.com/products/docker-desktop
+cask "firefox"              # https://www.mozilla.org/firefox/
+cask "iterm2"               # https://iterm2.com/
+cask "itsycal"              # https://www.mowglii.com/itsycal/
+cask "spotify"              # https://www.spotify.com/
+cask "imageoptim"           # https://imageoptim.com/mac
 EOF
 
 echo "Homebrew: Configuration is done"
