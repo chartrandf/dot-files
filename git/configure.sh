c86336ee861e343c7491230ae741817d4744b720
Francis Chartrand <chartrandf@users.noreply.github.com>
2024-04-19 12:11:57 -0400

    fix: gpg agent to stop asking key password

--- git/configure.sh
+++ git/configure.sh
@@ -13,5 +13,20 @@ chmod +x ~/.git-templates/hooks/commit-msg
 
 git config --global init.templatedir '~/.git-templates'
 
+# GPG agent configuration on M1, M2, M3 chips
+# @credit: https://quannhm.medium.com/signing-git-commits-on-mac-m1-a3d76170efce
+cat <<EOF > ~/.gnupg/gpg.conf
+use-agent
+pinentry-mode loopback
+EOF
+
+cat <<EOF > ~/.gnupg/gpg-agent.conf
+allow-preset-passphrase
+allow-loopback-pinentry
+EOF
+
+gpgconf --launch gpg-agent
+
+
 cd ..
 echo "GIT: Global configuration is done"
